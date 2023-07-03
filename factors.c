ca184a2ae998cf242b93170532cabc51c0813f42
Soukaina Samira <samira.merrakchi@hotmail.com>
2023-07-02 18:12:50 -0700

    sm

--- factors.c
+++ factors.c
@@ -1,33 +1,46 @@
-#include "factor.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
 
-/**
- * main - main function
- *
- *
- * Return: void
- */
 int main(int argc, char *argv[])
 {
-	FILE *fptr;
-	size_t count;
-	ssize_t line;
-	char *buffer = NULL;
+	FILE *stream;
+	char *line = NULL;
+	size_t len = 0;
+	long long flag = 1, div, rest, number, counter;
+	ssize_t nread;
 
-
-	if (argc != 2)
-	{
-		fprintf(stderr, "Usage: factor <filename>\n");
+	if (argc != 2) {
+		fprintf(stderr, "Usage: %s <file>\n", argv[0]);
 		exit(EXIT_FAILURE);
 	}
-	fptr = fopen(argv[1], "r");
-	if (fptr == NULL)
-	{
-		fprintf(stderr, "Error: can't open file %s\n", argv[1]);
+
+	stream = fopen(argv[1], "r");
+	if (stream == NULL) {
+		perror("fopen");
 		exit(EXIT_FAILURE);
 	}
-	while((line = getline(&buffer, &count, fptr)) != -1)
-	{
-		factorize(buffer);
+
+	while ((nread = getline(&line, &len, stream)) != -1) {
+		flag = 1, div = 2;
+		number = atoll(line);
+		while (flag) {
+			rest = number % div;
+			if (!rest) {
+				counter = number / div;
+				printf("%lld=%lld*%lld\n", number, counter, div);
+				flag = 0;
+			}
+			div++;
+		}
 	}
-	return (0);
+
+	free(line);
+	fclose(stream);
+	exit(EXIT_SUCCESS);
 }
